257075f7da37cc3f3c6ca314937480d1b4e966c8
barkley-wq <igonyibarkley00@gmail.com>
2024-08-06 08:57:06 +0300

    app.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,11 @@ import React from "react";
 import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
 import TestComponent from "./TestComponent"
-import { render, screen } from '@testing-library/react';
-import TestComponent from './TestComponent';
-test("should include the <TestComponent />", () => {
-  render(<TestComponent />);
-  const element = screen.getByText("time video");
-  expect(element).toBeInTheDocument();
-});
-const now = 'Now';
+
 function App() {
   return (
     <div className="App">
-      <h1>
-        {now}
-      </h1>
+      <h1>Now</h1>
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the
@@ -23,6 +14,7 @@ function App() {
         self-contained chunks of JSX.
       </p>
       <ExampleComponent />
+      <TestComponent />
     </div>
   );
 }
